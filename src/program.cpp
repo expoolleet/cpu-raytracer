4e99dfa9851236a6b660fa9dac681cc83c2392be
expool <gosha.shcherbak@mail.ru>
2026-04-24 17:28:00 +0300

    Added scene generation

--- src/program.cpp
+++ src/program.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 
+#include <bitset>
+
 #include "camera.hpp"
 #include "dialectric.hpp"
 #include "hittable_list.hpp"
@@ -11,29 +13,62 @@
 
 using std::make_shared;
 
+std::pair<float, float> hammersleySequence2D(uint32_t x, int n) {
+    // radical inverse vdc
+    uint32_t bits = x;
+    bits = (bits >> 16u) | (x << 16u);
+    bits = ((bits & 0xAAAAAAAA) >> 1u) | ((bits & 0x55555555) << 1u);
+    bits = ((bits & 0xCCCCCCCC) >> 2u) | ((bits & 0x33333333) << 2u);
+    bits = ((bits & 0xF0F0F0F0) >> 4u) | ((bits & 0x0F0F0F0F) << 4u);
+    bits = ((bits & 0xFF00FF00) >> 8u) | ((bits & 0x00FF00FF) << 8u);
+    return {static_cast<float>(x) / n, bits * 2.3283064365386963e-10f};  // / 0x1000000000
+}
+
 int main(int argc, char *argv[]) {
     HittableList world;
 
-    auto materialLambertian1 = make_shared<Lambertian>(color3(0.8f, 0.8f, 0.0f));
-    auto materialLambertian2 = make_shared<Lambertian>(color3(0.1f, 0.2f, 0.5f));
-    auto materialDialectric1 = make_shared<Dialectric>(color3(1.0f, 1.0f, 1.0f), 1.5f);
-    auto materialDialectric2 = make_shared<Dialectric>(color3(1.0f, 1.0f, 1.0f), 1.0f / 1.5f);
-    auto materialMetal1 = make_shared<Metal>(color3(0.8f, 0.6f, 0.2f), 0.3f);
+    float earthRadius = 100.0f;
+    int n = 60;
+    float scale = 5.5f;
+    float centerOffset = 2.0f;
+    float radiusOfCenteredMetalSpehere = 1.8f;
+    for (unsigned int i = 0; i < n; ++i) {
+        float rand = utils::random();
 
-    world.add(make_shared<Sphere>(point3(0.0f, -100.5f, -1.0f), 100.0f, materialLambertian1));
-    world.add(make_shared<Sphere>(point3(0.0f, 0.0f, -1.2f), 0.5f, materialLambertian2));
-    world.add(make_shared<Sphere>(point3(1.0f, 0.0f, -1.0f), 0.5f, materialMetal1));
-    world.add(make_shared<Sphere>(point3(-1.0f, 0.0f, -1.0f), 0.4f, materialDialectric2));
-    world.add(make_shared<Sphere>(point3(-1.0f, 0.0f, -1.0f), 0.5f, materialDialectric1));
+        float radius = utils::random(0.3f, 0.6f);
+        color3 color = color3(utils::random(), utils::random(), utils::random());
+        auto randPos = hammersleySequence2D(i, n);
+        float x = randPos.first * (2 * scale) - scale;
+        float z = randPos.second * (2 * scale) - scale;
+        float y = std::sqrtf(earthRadius * earthRadius - x * x - z * z) - earthRadius;
+        if ((x >= 0.0f && x <= centerOffset && z >= 0.0f && z <= centerOffset) || (x <= 0.0f && x >= -centerOffset && z <= 0.0f && z >= -centerOffset) ||
+            (x >= 0.0f && x <= centerOffset && z <= 0.0f && z >= -centerOffset) || (x <= 0.0f && x >= -centerOffset && z >= 0.0f && z <= centerOffset)) {
+            continue;
+        }
+        point3 position = point3(x, y + radius, z);
+        if (rand < 0.7f) {
+            world.add(make_shared<Sphere>(position, radius, make_shared<Lambertian>(color)));
+        } else if (rand < 0.95f) {
+            world.add(make_shared<Sphere>(position, radius, make_shared<Metal>(color, utils::random())));
+        } else {
+            world.add(make_shared<Sphere>(position, radius, make_shared<Dialectric>(color, utils::random(1.0f, 1.5f))));
+        }
+    }
+
+    auto materialLambertian1 = make_shared<Lambertian>(color3(0.8f, 0.8f, 0.8f));
+    auto materialMetal1 = make_shared<Metal>(color3(0.7f, 0.6f, 0.5f), 0.0f);
+
+    world.add(make_shared<Sphere>(point3(0.0f, -earthRadius, 0.0f), earthRadius, materialLambertian1));
+    world.add(make_shared<Sphere>(point3(0.0f, radiusOfCenteredMetalSpehere, 0.0f), radiusOfCenteredMetalSpehere, materialMetal1));
 
     Camera camera;
-    camera.position = point3(-2.0f, 2.0f, 1.0f);
-    camera.lookAt = point3(0.0f, 0.0f, -1.0f);
+    camera.position = point3(-5.0f, 3.0f, 7.0f);
+    camera.lookAt = point3(0.0f, 0.0f, 0.0f);
     camera.vUp = vec3(0.0f, 1.0f, 0.0f);
     camera.aspectRatio = 16.0f / 9.0f;
-    camera.vFov = 20.0f;
-    camera.defocusAngle = 10.0f;
-    camera.focusDistance = 3.4f;
+    camera.vFov = 50.0f;
+    camera.defocusAngle = 2.5f;
+    camera.focusDistance = 7.0f;
 
     if (argc > 3) {
         camera.imageWidth = atoi(argv[1]);
